337d2c881beb9cb254f2c5cf53eb4b8ba4c6ba2d
Terry Moore II <terry.moore.ii@gmail.com>
2017-11-04 14:49:24 -0400

    Init

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const Hapi = require('hapi');
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({ 
-    host: 'localhost', 
     port: process.env.port || 3000 
 });
 
 // Add the route
+server.route({
+    method: 'GET',
+    path:'/', 
+    handler: function (request, reply) {
+        return reply('hello world');
+    }
+});
+
 server.route({
     method: 'POST',
     path:'/slack/karma', 
