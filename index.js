0562625ad0158c78cd2bc582a2b67baf400300d0
Terry Moore II <terry.moore.ii@gmail.com>
2017-11-05 17:10:39 -0500

    Working

--- index.js
+++ index.js
@@ -79,40 +79,45 @@ function karmaSlashCommand(request, reply){
 }
 
 function getTop(payload) {
-    var karmaRef = db.collection('karma');
-    
-     return karmaRef.where('teamId', '==', payload.team_id).orderBy('karma', 'asc').limit(5).get().then(snapshot => {
-        var results = '';
-        snapshot.forEach(doc => {
-            const data = doc.data();
-            results += `${data.karma} ${data.userName}\n`
-        })
-        return response(results);
-    }).catch(error => console.log(error));
-
+    return getResults(payload, 'asc');
 }
 
 function getBottom(payload){
+   return getResults(payload, 'desc');
+}
+
+function getResults(payload, dir = 'asc'){
     var karmaRef = db.collection('karma');
     
-     return karmaRef.where('teamId', '==', payload.team_id).orderBy('karma', 'desc').limit(5).get().then(snapshot => {
-        var results = '';
+     return karmaRef.where('teamId', '==', payload.team_id).orderBy('karma', dir).limit(5).get().then(snapshot => {
+        
+        let results = []; 
         snapshot.forEach(doc => {
-            const data = doc.data();
-            results += `${data.karma} ${data.userName}\n`
-        })
-        return response(results);
+            results.push( doc.data());
+        });
+
+        const promises = results.map(person => getUserInfo(person.userId));
+
+        return Promise.all(promises).then(users => {
+            results = results.map((person, index) => {
+                person.user = users[index];
+                return person;
+            })
+            return results;
+        }).then(results => {
+            let message = '';
+            results.forEach(result => {
+                message += `${result.karma}  ${result.user.user.real_name}`;
+            })
+            return message;
+        }).then(message => response(message));
+
     }).catch(error => console.log(error));
 }
 
 function help(message = '') {
     return `${message} \n
     Available /karma Commands:
-    /karama <@user>   Give or remove karma with pluses or minuses
-                      The more pluses or minues the more or less karma!
-                      examples: 
-                          /karma @tmoore +++
-                          /karam @tmoore -- 
     /karma top        Show Top Karma Users
     /karma bottom     Show Bottom Karma Users
     `;
@@ -125,42 +130,6 @@ function response(message) {
     }
 }
 
-// function karma(payload, text) {
-//     let [user, karma] = text.split(' ');
-//     let count = 0;
-
-//     if (user.includes('+')) {
-//         var index = user.indexOf('+');
-//         karma = user.substr(index);
-//         user = user.substr(0, index);
-//     } else if (user.includes('-')) {
-//         var index = user.indexOf('-');
-//         karma = user.substr(index);
-//         user = user.substr(0, index);
-//     }
-
-//     if (!karma || (!karma.startsWith('++') && !karma.startsWith('--'))) {
-//         return help('Add either pluses or minuses after the user\'s name!');
-//     }
-
-//     if (user.substr(1) === payload.user_name) {
-//         return response(`${user} tried to give themself karma.`);
-//     }
-
-//     if (karma.length > 5) {
-//         count = 5;
-//      } else {
-//         count = karma.length - 1;
-//     }
-
-//     if (karma[0] === '-') {
-//         count = -count;        
-//     } 
-
-//     const direction = count < 0 ? 'removed' : 'gave';
-//     return setDbKarma(payload, user, count)
-//         .then(karma => response(`${payload.user_name} ${direction} ${user} ${count} karma. They now have ${karma} karma.`));
-// }
 
 function getDbKarma(person) {
     return db.collection('karma').doc(person.userId);
@@ -224,30 +193,20 @@ function handleRtmMessage(message) {
 
     const savePromises = karma.map(person => setDbKarma(person))
     Promise.all(savePromises).then(result => {
-        console.log('result1')
-        console.log(result)
         karma = karma.map((person, index) => {
             person.total = result[index];
             return person;
         });
-        console.log('save promises');
-        console.log(karma)
         return karma
     }).then((karma) => {
         var usersPromises = karma.map(person => getUserInfo(person.userId))
         Promise.all(usersPromises).then(result => {
-            console.log('result2')
-            console.log(result)
             karma = karma.map((person, index) => {
                 person.user = result[index];
                 return person;
             });
-            console.log('user promises');
-            console.log(karma)
             return karma;
         }).then((karma) => {
-            console.log('before send message');
-            console.log(karma)
             sendMessage(karma, channel);
         });
     });
@@ -256,51 +215,8 @@ function handleRtmMessage(message) {
 function sendMessage(karma, channel) {
     var message = '';
     karma.forEach(person => {
-        console.log(person.user);
         message += `${person.user.user.real_name} karma was ${person.karma > 0 ? 'increased' : 'decresed'} to ${person.total} `
     })
 
     rtm.sendMessage(message, channel);
 }
-
-/*
-
-parse for karma
-save to db and get new karma amount
-get use names 
-send message
-
-
-*/
-
-
-
-
-/*
-
-
-
-{ 
-   token: '5SFzFzk3lcvfUzXbeYXG0Phe',
-   team_id: 'T3QDS4TNY',
-   team_domain: 'broadvine',
-   channel_id: 'G7V3SMHFE',
-   channel_name: 'privategroup',
-   user_id: 'U5G9H3N8J',
-   user_name: 'tmoore',
-   command: '/karma',
-   text: 'top',
-   response_url: 'https://hooks.slack.com/commands/T3QDS4TNY/267162849044/KUYCHqCUeUVeJhp529AB9J76',
-   trigger_id: '266596392065.126468163780.3c12c4452f6d8b1a0b201eaeed74102f' 
-}
-
-{ type: 'message',
-2017-11-05T19:15:13.500627+00:00 app[web.1]:   channel: 'C7V8F4S8L',
-2017-11-05T19:15:13.500628+00:00 app[web.1]:   user: 'U5G9H3N8J',
-2017-11-05T19:15:13.500629+00:00 app[web.1]:   text: 'test',
-2017-11-05T19:15:13.500629+00:00 app[web.1]:   ts: '1509909313.000040',
-2017-11-05T19:15:13.500630+00:00 app[web.1]:   source_team: 'T3QDS4TNY',
-2017-11-05T19:15:13.500630+00:00 app[web.1]:   team: 'T3QDS4TNY' }
-
-
-*/
