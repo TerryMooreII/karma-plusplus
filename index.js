60a71036d4a6abf08f4ddcf797521b6b0b7e4337
Terry Moore II <terry.moore.ii@gmail.com>
2017-11-05 10:29:46 -0500

    Add rtm

--- index.js
+++ index.js
@@ -13,18 +13,25 @@ const db = admin.firestore();
 
 
 var RtmClient = require('@slack/client').RtmClient;
-var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
-var bot_token = process.env.SLACK_BOT_TOKEN || '';
+var bot_token = process.env.SLACK_API_TOKEN || '';
 
-var rtm = new RtmClient(bot_token);
+var rtm = new RtmClient(bot_token, { logLevel: 'info' });
+rtm.start();
 
-// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+rtm.on(RTM_EVENTS.MESSAGE, function(message) {  
+    var channel = message.channel;
+    var text = message.text;
+    rtm.sendMessage(text, channel);
 });
 
-rtm.start();
+// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
+// rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+//   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+// });
+
+
 
 
 // Create a server with a host and port
